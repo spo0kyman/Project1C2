26fd64f12e1c6d13f1fe559f82e0bb81bd4bcdc2
spo0kyman <61479588+spo0kyman@users.noreply.github.com>
2020-06-29 16:57:19 -0400

    making program work

--- Project1C2/Project1C2.cpp
+++ Project1C2/Project1C2.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include "core.h"
+
+bool Update(float dt)
+{
+	return false;
+}
+
+void Draw(Core::Graphics& graphics)
+{
+	graphics.SetColor(RGB(rand() % 256, rand() % 256, rand() % 256));
+	graphics.DrawLine(static_cast<float>(rand() % 800), 
+		static_cast<float>(rand() % 600),
+		static_cast<float>(rand() % 800),
+		static_cast<float>(rand() % 600));
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+	char name[] = "CSC196";
+	Core::Init(name, 800, 600);
+	Core::RegisterUpdateFn(Update);
+	Core::RegisterDrawFn(Draw);
 
+	Core::GameLoop();
+	Core::Shutdown();
+}
